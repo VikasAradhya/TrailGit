1ee42bef3f0c4f7b765e7543c0416e88f8d3caac
aradh <aradh@aradhya>
2022-01-15 13:55:12 +0530

    My First Git

---
+++ TestMaven/src/test/java/Sample/Test.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
